9549d235a069424550d800fcf9747683ce26e61f
AmeerDesginer <amersosa23@gmail.com>
2019-09-08 14:51:01 +0300

    Update md5.php

--- md5.php
+++ md5.php
@@ -6,37 +6,23 @@ $deb = $_GET['deb'];
 
 $path = "api/debs/$deb"; 
 
-
+// get file deb 
 $file1 = basename($path); 
 $file2 = basename($path, ".php"); 
+// Get SHA1 type
 $sha1file = sha1_file($path);
+// Get SHA256 type
 $sha256 = hash_file("sha256", $path);
-//Show filename with file extension 
-// echo $file1 . "\n"; 
-
-
-
-//Show filename without file extension 
-// echo $file2;
 
 
 echo 'File name: '. $file1 .' <br>';
+// Get MD5 type
 echo 'MD5 file: ' . md5_file($path).' <br>';
 
+// Get size file bytes
 echo 'File size: ' . filesize($path) . ' bytes <br>';
 echo 'SHA1 file: ' . $sha1file . '<br>';
 echo 'SHA256 file: ' . $sha256 . '<br>';
-
-
-// $zip = new ZipArchive;
-// $res = $zip->open('api/debs/1.zip');
-// if ($res === TRUE) {
-//   $zip->extractTo('/debex/');
-//   $zip->close();
-//   echo 'woot!';
-// } else {
-//   echo 'doh!';
-// }
 
 
 ?>
